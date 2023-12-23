25d4057c1a4fdff2c3dae55ac2701b65da0005dd
osparking <jbpark03@gmail.com>
2023-12-23 11:38:43 +0900

    영희가 구현한 Flight(항공편) 클래스

---
+++ src/main/java/space/bum/junit/ch13/flight/Flight.java
@@ -0,0 +1,46 @@
+package space.bum.junit.ch13.flight;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import space.bum.junit.ch13.passen.Passenger;
+
+public class Flight {
+  private String flightNumber;
+  private int seats;
+  private Set<Passenger> passengers = new HashSet<>();
+
+  private static String flightNumberRegex = "^[A-Z]{2}\\d{3,4}$";
+  private static Pattern pattern = Pattern.compile(flightNumberRegex);
+
+  public Flight(String flightNumber, int seats) {
+    super();
+    Matcher matcher = pattern.matcher(flightNumber);
+    if (!matcher.matches()) {
+      throw new RuntimeException("잘못된 형식의 항공편 번호:" + flightNumber);
+    }
+    this.flightNumber = flightNumber;
+    this.seats = seats;
+  }
+
+  public String getFlightNumber() {
+    return flightNumber;
+  }
+
+  public int getNumberOfPassengers() {
+    return passengers.size();
+  }
+  
+  public boolean addPassengers(Passenger passenger) {
+    if (getNumberOfPassengers() >= seats) {
+      throw new RuntimeException("좌석이 부족한 항공편 번호: " + flightNumber);
+    }
+    return passengers.add(passenger);
+  }
+
+  public boolean removePassenger(Passenger passenger) {
+    return passengers.remove(passenger);
+  }
+}
