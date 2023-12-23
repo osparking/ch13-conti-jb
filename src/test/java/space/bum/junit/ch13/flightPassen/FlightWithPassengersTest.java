6a6209dc10a14a08ab8083f776ae7419c3d5814a
osparking <jbpark03@gmail.com>
2023-12-23 19:25:28 +0900

    Flight 와 Passenger 의 집적 시험 - 2 건

---
+++ src/test/java/space/bum/junit/ch13/flightPassen/FlightWithPassengersTest.java
@@ -0,0 +1,38 @@
+package space.bum.junit.ch13.flightPassen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import space.bum.junit.ch13.flight.Flight;
+import space.bum.junit.ch13.passen.Passenger;
+
+class FlightWithPassengersTest {
+  private Flight flight = new Flight("AF1234", 1);
+
+  @Test
+  void testAddRemovePassengers() throws IOException {
+    Passenger passenger = new Passenger("123-456-789", "정다래", "KR");
+    assertTrue(flight.addPassengers(passenger));
+    assertEquals(1, flight.getNumberOfPassengers());
+
+    assertTrue(flight.removePassenger(passenger));
+    assertEquals(0, flight.getNumberOfPassengers());
+  }
+
+  @Test
+  void testNumberOfSeats() throws IOException {
+    Passenger passenger = new Passenger("123-456-789", "정다래", "KR");
+
+    flight.addPassengers(passenger);
+    assertEquals(1, flight.getNumberOfPassengers());
+
+    Passenger passenger2 = new Passenger("230-456-789", "다나카", "JP");
+    assertThrows(RuntimeException.class,
+        () -> flight.addPassengers(passenger2));
+  }
+}
