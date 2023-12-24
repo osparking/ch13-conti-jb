e85a16c4161e858f3891386ae85348f657d606b4
osparking <jbpark03@gmail.com>
2023-12-24 10:39:26 +0900

    승객에 항공편을, 항공편에 승객을...

--- src/main/java/space/bum/junit/ch13/passen/Passenger.java
+++ src/main/java/space/bum/junit/ch13/passen/Passenger.java
@@ -4,14 +4,34 @@ import java.util.Arrays;
 import java.util.Locale;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import space.bum.junit.ch13.flight.Flight;
 
 @Getter
+@Setter
 @ToString
 public class Passenger {
   private String identifier;
   private String name;
   private String countryCode;
+  
+  private Flight flight;
+
+  public void joinFlight(Flight flight) {
+    Flight prev = this.flight;
+    if (prev != null) {
+      if (!prev.removePassenger(this)) {
+        throw new RuntimeException("승객 제거 실패");
+      }
+    }
+    setFlight(flight);
+    if (flight != null) {
+      if (!flight.addPassengers(this)) {
+        throw new RuntimeException("항공편 승객 추가 실패");
+      }
+    }
+  }
 
   public Passenger(String identifier, String name, String countryCode) {
     super();
